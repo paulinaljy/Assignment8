637e174eff2e6e5b8d023af4d32395a749e7fe1b
Paulina Lee <paulinalee718@gmail.com>
2025-04-11 14:42:12 -0400

    commit before pull

--- bowen/src/event/ModelStatus.java
+++
@@ -1,9 +0,0 @@
-package event;
-
-/**
- * Represents different status changes that can occur in the model.
- */
-public enum ModelStatus {
-  TURN_CHANGED,  // When the active player switches
-  GAME_OVER     // When the game ends
-} 
